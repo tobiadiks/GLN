d7c783ae2a4a60dd2e8a5ab29dca5c724a9c8416
tobiadiks <tobiadiks@gmail.com>
2020-06-22 23:51:01 +0100

    second

--- glifeweb/settings.py
+++ glifeweb/settings.py
@@ -30,7 +30,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'ww($t$8dr30+%4ew9r2g3e6fzq$jkky(=hc_p3#470r@yivzez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['glnation.herokuapp.com']
 
